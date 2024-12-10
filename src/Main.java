bd15d72b18af2488be7110ac2fd351635f774b87
Abbas49 <abbasabdo735@gmail.com>
2024-12-10 18:52:23 +0200

    format the code and fix some of the errors

--- src/Main.java
+++ src/Main.java
@@ -3,42 +3,43 @@ import java.util.*;
 // while validating input if the input is invalid, tell the user that, and ask him to enter the inputs again
 class InputHandler {
     public static Scanner scan = new Scanner(System.in);
+
     // scan and return the secretCode size
     public static int getSecretSize() {
         System.out.print("Enter the size of the Secret code: at least 3");
         int size = scan.nextInt();
-        if(size < 3){
+        if (size < 3) {
             System.out.println("Secret code must be at least 3");
             return getSecretSize();
-        }else return size;
+        } else return size;
     }
 
-    private static String getValidGuessInput(int size){
-        System.out.println("Enter  r, g, y, b, o, p : ");
-        String str = scan.next();
+    private static String getValidGuessInput(int size) {
+        System.out.println("Enter Guess: ");
+        String str = scan.nextLine();
         str = str.toLowerCase();
-        if(str.length() != size){
+        if (str.length() != size) {
             System.out.println("You should input " + size + " characters, try again.");
             return getValidGuessInput(size);
         }
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             char c = str.charAt(i);
-            if(c != 'r' && c!='g' && c!= 'y' && c!='b' && c!= 'o' &&c!='p'){
+            if (c != 'r' && c != 'g' && c != 'y' && c != 'b' && c != 'o' && c != 'p') {
                 System.out.println("The character " + c + " is not valid, try again.");
                 return getValidGuessInput(size);
             }
         }
         return str;
     }
+
     public static Guess getGuess(int size) {
-        System.out.print("Enter the guss: ");
-        Guess guess= new Guess(getSecretSize()) ;
+        Guess guess = new Guess(getSecretSize());
         String str = getValidGuessInput(size);
-        for(int i = 0; i < size; i++){
-             char c = str.charAt(0);
-             guess.setColor(i, c);
+        for (int i = 0; i < size; i++) {
+            char c = str.charAt(0);
+            guess.setColor(i, c);
         }
-        return  guess;
+        return guess;
     }
 }
 
@@ -46,60 +47,71 @@ class OutputHandler {
 
 }
 
-class GuessResult{
+class GuessResult {
     public int blackBalls = 0;
     public int whiteBalls = 0;
-    GuessResult(int blackBalls, int whiteBalls){
+
+    GuessResult(int blackBalls, int whiteBalls) {
         this.blackBalls = blackBalls;
         this.whiteBalls = whiteBalls;
     }
 }
-class MasterMind{
+
+class MasterMind {
     SecretColors secretCode;
-    MasterMind(){
+
+    MasterMind() {
         System.out.println("Enter the size of the Code");
-        Scanner s = new Scanner(System.in);
-        int size = s.nextInt();
+        int size = InputHandler.getSecretSize();
         secretCode = new SecretColors(size);
+        guessCode();
     }
-    public void guessNumber(){
+
+    public void guessCode() {
         Guess guess = InputHandler.getGuess(secretCode.getSize());
         GuessResult result = validateGuess(guess);
+        System.out.println("black: " + result.blackBalls);
+        System.out.println("white: " + result.whiteBalls);
     }
-    private GuessResult validateGuess(Guess guess){
+
+    private GuessResult validateGuess(Guess guess) {
         GuessResult result = new GuessResult(0, 0);
         result.blackBalls = numberOfPositionMatch(guess);
         result.whiteBalls = numberOfColorMatch(guess);
         return result;
     }
-    public void restartGame(){
+
+    public void restartGame() {
 
     }
-    public int numberOfPositionMatch(Guess guess){
+
+    public int numberOfPositionMatch(Guess guess) {
         String guessStr = guess.toString();
         String secretStr = guess.toString();
         int count = 0;
-        for(int i = 0; i < secretStr.length(); i++) {
-            if(guessStr.charAt(i) == secretStr.charAt(i)){
+        for (int i = 0; i < secretStr.length(); i++) {
+            if (guessStr.charAt(i) == secretStr.charAt(i)) {
                 count++;
             }
         }
         return count;
     }
-    public int numberOfColorMatch(Guess guess){
+
+    public int numberOfColorMatch(Guess guess) {
         int[] guessColorsCount = new int[Color.colorsCount];
         int[] secretColorsCount = new int[Color.colorsCount];
-        for(int i = 0; i < guess.getSize(); i++){
+        for (int i = 0; i < guess.getSize(); i++) {
             guessColorsCount[guess.getColor(i).id]++;
             secretColorsCount[secretCode.getColor(i).id]++;
         }
         int count = 0;
-        for(int i = 0; i < guess.getSize(); i++){
+        for (int i = 0; i < guess.getSize(); i++) {
             count += Math.min(guessColorsCount[i], secretColorsCount[i]);
         }
         return count;
     }
 }
+
 class Color {
     public static Color red, green, yellow, blue, orange, purple;
     public static int colorsCount = 0;
@@ -111,9 +123,11 @@ class Color {
         this.id = id;
         this.name = name;
     }
+
     public String toString() {
         return name;
     }
+
     // color
     static {
         red = new Color(0, "Red");
@@ -124,108 +138,118 @@ class Color {
         purple = new Color(5, "Purple");
     }
 }
-class Guess{
+
+class Guess {
     protected Color[] colors;
-    protected static final Color[] COLORS ={
-            Color.red,Color.green,Color.yellow,Color.blue,Color.orange,Color.purple
+    protected static final Color[] COLORS = {
+            Color.red, Color.green, Color.yellow, Color.blue, Color.orange, Color.purple
     };
-    public Guess(int size){
+
+    public Guess(int size) {
         colors = new Color[size];
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("your guess");
-            String input = scanner.nextLine().toLowerCase(null);
-        
-        if (validateInput(input , size)) {
-            for(int i = 0 ; i < size ; i++){
-                colors[i] = charToColor(input.charAt(i));
-            }
-        break;
-        }else{
-            System.out.println("invslid input");
-        }
-        
-        }
+//        Scanner scanner = new Scanner(System.in);
+//        while (true) {
+//            System.out.println("your guess");
+//            String input = scanner.nextLine().toLowerCase(null);
+//
+//        if (validateInput(input , size)) {
+//            for(int i = 0 ; i < size ; i++){
+//                colors[i] = charToColor(input.charAt(i));
+//            }
+//        break;
+//        }else{
+//            System.out.println("invslid input");
+//        }
+//
+//        }
     }
-    protected Color charToColor(char c){
+
+    protected Color charToColor(char c) {
         switch (c) {
             case 'r':
                 return Color.red;
-                case 'g':
+            case 'g':
                 return Color.green;
-                case 'b':
+            case 'b':
                 return Color.blue;
-                case 'o':
+            case 'o':
                 return Color.orange;
-                case 'p':
+            case 'p':
                 return Color.purple;
-                case 'y':
+            case 'y':
                 return Color.yellow;
-    
+
             default:
-            throw new IllegalArgumentException("invalid char"+c);
+                throw new IllegalArgumentException("invalid char" + c);
         }
     }
-    protected boolean validateInput(String input, int size){
-        if (input.length() != size)return false;
-          for (char c : input.toCharArray()){
-            if("rgbopy".indexOf(c) == -1) return false;
-          }
-            return true;
-        
+
+    protected boolean validateInput(String input, int size) {
+        if (input.length() != size) return false;
+        for (char c : input.toCharArray()) {
+            if ("rgbopy".indexOf(c) == -1) return false;
+        }
+        return true;
     }
+
     // return the string of the colors chars (no space in between)
-    public String toString(){
-       StringBuilder sb = new StringBuilder();
-       for(Color color : colors){
-        sb.append(color.symbol);
-       }
-    return sb.toString();
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Color color : colors) {
+            sb.append(color.symbol);
+        }
+        return sb.toString();
     }
+
     // return the char of the color at the position n
-    public char getChar(int n){
-       if (n>=0 && n< colors.length) {
-         return colors[n].symbol;
-       }
-       throw new IndexOutOfBoundsException("Invalid index color");
+    public char getChar(int n) {
+        if (n >= 0 && n < colors.length) {
+            return colors[n].symbol;
+        }
+        throw new IndexOutOfBoundsException("Invalid index color");
     }
+
     // return the size of the secret code
-    public int getSize(){
+    public int getSize() {
         return colors.length;
     }
+
     // return the object color of index n
-    public Color getColor(int n){
+    public Color getColor(int n) {
         if (n >= 0 && n < colors.length) {
             return colors[n];
         }
         throw new IndexOutOfBoundsException("Invalid index color");
     }
-    public void setColor(int index, char colorChar){
+
+    public void setColor(int index, char colorChar) {
         Color color = charToColor(colorChar);
         colors[index] = color;
     }
 
-    Color[] getColors()
-    {
+    Color[] getColors() {
         return colors;
     }
 }
-class SecretColors extends Guess{
+
+class SecretColors extends Guess {
     public SecretColors(int size) {
         super(size);
         generateRandom();
     }
-    private void generateRandom(){
-     Random random = new Random();
- 
-   for(int i = 0 ; i < colors.length ; i++){
-   colors[i]=COLORS[random.nextInt(COLORS.length)];}
-   }
+
+    private void generateRandom() {
+        Random random = new Random();
+
+        for (int i = 0; i < colors.length; i++) {
+            colors[i] = COLORS[random.nextInt(COLORS.length)];
+        }
+    }
 }
 
 
 public class Main {
     public static void main(String[] args) {
-
+        MasterMind m = new MasterMind();
     }
 }
