847724f5960d742bbe49a8d56814f8ecd4425e82
Abbas49 <abbasabdo735@gmail.com>
2024-12-10 14:54:45 +0200

    guess validation + add tasks

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,37 @@
 import java.util.*;
 
+// while validating input if the input is invalid, tell the user that, and ask him to enter the inputs again
 class InputHandler {
     public static Scanner scan = new Scanner(System.in);
-
+    // scan and return the secretCode size
     public static int getSecretSize() {
         System.out.print("Enter the size of the Secret code: at least 3");
         int size = scan.nextInt();
         return size;
     }
-
-    public static String getGuess() {
-        return "";
-    }
-
-    public static boolean validateGuess() {
-        return false;
+    /*
+    scan input and make sure it is correct input
+    - right size
+    - right form
+    - right chars
+     */
+    public static Guess getGuess(int size) {
+        return new Guess(size);
     }
 
 }
 
 class OutputHandler {
 
+}
+
+class GuessResult{
+    public int blackBalls = 0;
+    public int whiteBalls = 0;
+    GuessResult(int blackBalls, int whiteBalls){
+        this.blackBalls = blackBalls;
+        this.whiteBalls = whiteBalls;
+    }
 }
 class MasterMind{
     SecretColors secretCode;
@@ -30,24 +41,50 @@ class MasterMind{
         int size = s.nextInt();
         secretCode = new SecretColors(size);
     }
-    void guessNumber(){
+    public void guessNumber(){
+        Guess guess = InputHandler.getGuess(secretCode.getSize());
+        GuessResult result = validateGuess(guess);
+    }
+    private GuessResult validateGuess(Guess guess){
+        GuessResult result = new GuessResult(0, 0);
+        result.blackBalls = numberOfPositionMatch(guess);
+        result.whiteBalls = numberOfColorMatch(guess);
+        return result;
+    }
+    public void restartGame(){
 
     }
-    void restartGame(){
-
+    public int numberOfPositionMatch(Guess guess){
+        String guessStr = guess.toString();
+        String secretStr = guess.toString();
+        int count = 0;
+        for(int i = 0; i < secretStr.length(); i++) {
+            if(guessStr.charAt(i) == secretStr.charAt(i)){
+                count++;
+            }
+        }
+        return count;
     }
-    int numberOfPositionMatch(){
-        return 0;
-    }
-    int numberOfColorMatch(){
-        return 0;
+    public int numberOfColorMatch(Guess guess){
+        int[] guessColorsCount = new int[Color.colorsCount];
+        int[] secretColorsCount = new int[Color.colorsCount];
+        for(int i = 0; i < guess.getSize(); i++){
+            guessColorsCount[guess.getColor(i).id]++;
+            secretColorsCount[secretCode.getColor(i).id]++;
+        }
+        int count = 0;
+        for(int i = 0; i < guess.getSize(); i++){
+            count += Math.min(guessColorsCount[i], secretColorsCount[i]);
+        }
+        return count;
     }
 }
 class Color {
     public static Color red, green, yellow, blue, brown, purple;
-
+    public static int colorsCount = 0;
     public int id;
     public String name;
+    public char symbol;
 
     Color(int id, String name) {
         this.id = id;
@@ -66,24 +103,45 @@ class Color {
         purple = new Color(5, "Purple");
     }
 }
-class SecretColors {
-    private Color[] colors; 
-    private static final Color[] COLORS ={
-    Color.red,Color.green,Color.yellow,Color.blue,Color.brown,Color.purple
-    }; 
-    public SecretColors(int size){
-    colors = new Color[size];
-      generateRandom();}
-    
+class Guess{
+    protected Color[] colors;
+    protected static final Color[] COLORS ={
+            Color.red,Color.green,Color.yellow,Color.blue,Color.brown,Color.purple
+    };
+    public Guess(int size){
+        colors = new Color[size];
+    }
+    // return the string of the colors chars (no space in between)
+    public String toString(){
+        return "";
+    }
+    // return the char of the color at the position n
+    public char getChar(int n){
+        return 't';
+    }
+    // return the size of the secret code
+    public int getSize(){
+        return 0;
+    }
+    // return the object color of index n
+    public Color getColor(int n){
+        return colors[n];
+    }
+    Color[] getColors()
+    {
+        return colors;
+    }
+}
+class SecretColors extends Guess{
+    public SecretColors(int size) {
+        super(size);
+        generateRandom();
+    }
     private void generateRandom(){
      Random random = new Random();
  
    for(int i = 0 ; i < colors.length ; i++){
    colors[i]=COLORS[random.nextInt(COLORS.length)];}
-   }
-   Color[] getSecrateCode()
-   {
-        return colors;
    }
 }
 
