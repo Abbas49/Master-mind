7093614b2404b0cde17180356f4c67c9965f6487
Abbas49 <abbasabdo735@gmail.com>
2024-12-12 18:37:58 +0200

    add GUI javaFX

--- src/GuessResult.java
+++
@@ -1,9 +0,0 @@
-class GuessResult {
-    public int blackBalls = 0;
-    public int whiteBalls = 0;
-
-    GuessResult(int blackBalls, int whiteBalls) {
-        this.blackBalls = blackBalls;
-        this.whiteBalls = whiteBalls;
-    }
-}
